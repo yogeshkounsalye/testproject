95b2532086d4535544db6dfa7d57c0c015da83ed
mahes <mahes@192.168.1.30>
2022-11-16 00:13:49 +0530

    second commit

---
+++ testproject/src/testproject/test.java
@@ -0,0 +1,15 @@
+package testproject;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("TCO1");
+		System.out.println("TCO1");
+		System.out.println("TCO1");
+		System.out.println("TCO1");
+		System.out.println("TCO1");
+		System.out.println("TCO1");
+		
+		
+	}
+
+}
